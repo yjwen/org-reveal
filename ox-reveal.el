bc2ae23f448a88a6f8e12b4542c2b89f3678354e
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2019-01-02 21:01:33 +0100

    Get rid of free variable retfile
    I never used multiplexing.
    Hopefully, this commit does not break anything.

--- ox-reveal.el
+++ ox-reveal.el
@@ -1552,17 +1552,16 @@ transformed fragment attribute to ELEM's attr_html plist."
          (file (org-export-output-file-name extension subtreep))
          (clientfile (org-export-output-file-name (concat "_client" extension) subtreep)))
 
-    ; export filename_client HTML file if multiplexing
     (setq client-multiplex nil)
-    (setq retfile (org-export-to-file 'reveal file
-                    async subtreep visible-only body-only ext-plist))
+    (org-export-to-file 'reveal file
+      async subtreep visible-only body-only ext-plist)
 
-    ; export the client HTML file if client-multiplex is set true
-    ; by previous call to org-export-to-file
-    (if (eq client-multiplex t)
+    ;; Export the client HTML file if client-multiplex is set true
+    ;; by previous call to org-export-to-file
+    (if client-multiplex
         (org-export-to-file 'reveal clientfile
           async subtreep visible-only body-only ext-plist))
-    (cond (t retfile))))
+    file))
 
 (defun org-reveal-export-to-html-and-browse
   (&optional async subtreep visible-only body-only ext-plist)
